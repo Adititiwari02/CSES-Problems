6e3bec3a02fb22c1439bd03f231d9d34b0be5227
Aditi Tiwari <tiwariaadi.18@gmail.com>
2021-11-02 19:38:04 +0530

    Day 3 Solutions

---
+++ Introductory Problems/BitString.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+typedef long long ll;
+const ll mod = 1e9 + 7;
+#define rep(i,a,b) for (int i = a; i < b; i++)
+
+// very important question
+// modular exponentiation
+
+ll power(ll x, ll y, ll p) {
+    // (x^y) mod p
+    ll res = 1;
+    x %= p;
+    while (y) {
+        if (y & 1)
+            res = (res * x) % p;
+        y >>= 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n;
+    cin >> n;
+    cout << power(2, n, mod) << endl;
+    return 0;
+}
---
+++ Introductory Problems/TrailingZeros.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+typedef long long ll;
+const ll mod = 1e9 + 7;
+#define rep(i,a,b) for (int i = a; i < b; i++)
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n;
+    cin >> n;
+    ll n1 = n, ans = 0;
+    while (n1 >= 5) {
+        n1 /= 5;
+        ans += n1;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
